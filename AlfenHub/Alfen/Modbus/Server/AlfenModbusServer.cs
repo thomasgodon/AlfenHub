35eee0979d5dc937241087e657df1d597cac13d0
Thomas Godon <thomas.godon@gmail.com>
2024-04-09 09:09:30 +0200

    add modbus server

--- AlfenHub/Alfen/Modbus/Server/AlfenModbusServer.cs
+++ AlfenHub/Alfen/Modbus/Server/AlfenModbusServer.cs
@@ -1,9 +1,48 @@
-﻿namespace AlfenHub.Alfen.Modbus.Server;
+﻿using FluentModbus;
+using Microsoft.Extensions.Logging;
+
+namespace AlfenHub.Alfen.Modbus.Server;
 
 internal class AlfenModbusServer : IAlfenModbusServer
 {
-    public Task Start(CancellationToken cancellationToken)
+    private readonly ILogger<AlfenModbusServer> _logger;
+    private readonly ModbusTcpServer _modbusServer;
+
+    public AlfenModbusServer(ILogger<AlfenModbusServer> logger)
     {
-        throw new NotImplementedException();
+        _logger = logger;
+        _modbusServer = new ModbusTcpServer(_logger, isAsynchronous: true);
+    }
+
+    public async Task Start(CancellationToken cancellationToken)
+    {
+        _modbusServer.Start();
+        _modbusServer.RegistersChanged += async (_, args) =>
+        {
+            await RegistersChanged(args, cancellationToken);
+        };
+        var serverLock = _modbusServer.Lock;
+
+        _logger.LogInformation("Modbus started at port 502");
+
+        while (cancellationToken.IsCancellationRequested is false)
+        {
+            // add mediatr stuff
+
+            // update server register content only once per second
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+        }
+
+        _modbusServer.Dispose();
+        _logger.LogWarning("Modbus stopped");
+    }
+
+    private Task RegistersChanged(RegistersChangedEventArgs e, CancellationToken cancellationToken)
+    {
+        foreach (var register in e.Registers)
+        {
+            _logger.LogTrace("Register changed: {register}", register);
+        }
+        return Task.CompletedTask;
     }
 }
