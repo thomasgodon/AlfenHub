d0949c3be440cde65e8003691e90a9fdf9c00c2a
Thomas Godon <thomas.godon@gmail.com>
2024-04-23 23:30:10 +0200

    convert to float

--- AlfenHub/Alfen/Modbus/Server/AlfenModbusReader.cs
+++ AlfenHub/Alfen/Modbus/Server/AlfenModbusReader.cs
@@ -4,19 +4,44 @@ namespace AlfenHub.Alfen.Modbus.Server;
 
 internal partial class AlfenModbusClient
 {
-    private const byte UnitIdentifier = 0x00;
+    private const int StationIdentifier = 200;
 
     private async Task<AlfenData> GetAlfenModbusData(CancellationToken cancellationToken) =>
         new()
         {
-            StationActiveMaxCurrent = await GetStationActiveMaxCurrentAsync(cancellationToken)
+            StationActiveMaxCurrent = await GetStationActiveMaxCurrentAsync(cancellationToken),
+            Temperature = await GetTemperatureAsync(cancellationToken),
+            TotalSockets = await GetTotalSocketsAsync(cancellationToken)
         };
 
-    private async Task<double> GetStationActiveMaxCurrentAsync(CancellationToken cancellationToken)
+    private async Task<float> GetStationActiveMaxCurrentAsync(CancellationToken cancellationToken)
     {
         const ushort startingAddress = 1100;
         const ushort count = 2;
-        var data = await _modbusClient.ReadInputRegistersAsync<ushort>(UnitIdentifier, startingAddress, count, cancellationToken);
-        return Math.Round((data.ToArray()[1] << 16 | data.ToArray()[0] & 0xffff) * 0.1, 2);
+        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
+        return ConvertToFloat(data);
+    }
+
+    private async Task<float> GetTemperatureAsync(CancellationToken cancellationToken)
+    {
+        const ushort startingAddress = 1102;
+        const ushort count = 2;
+        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
+        return ConvertToFloat(data);
+    }
+
+    private async Task<ushort> GetTotalSocketsAsync(CancellationToken cancellationToken)
+    {
+        const ushort startingAddress = 1105;
+        const ushort count = 1;
+        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
+        return data.ToArray()[0];
+    }
+
+    private static float ConvertToFloat(Memory<ushort> data)
+    {
+        var bytes = BitConverter.GetBytes(data.ToArray()[1])
+            .Concat(BitConverter.GetBytes(data.ToArray()[0])).ToArray();
+        return BitConverter.ToSingle(bytes);
     }
 }
--- AlfenHub/Alfen/Models/AlfenData.cs
+++ AlfenHub/Alfen/Models/AlfenData.cs
@@ -3,4 +3,6 @@
 internal class AlfenData
 {
     public double StationActiveMaxCurrent { get; init; } = default!;
+    public double Temperature { get; init; } = default!;
+    public ushort TotalSockets { get; init; } = default!;
 }
