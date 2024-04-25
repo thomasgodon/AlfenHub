a3c4eaebedf9aa5dcfd9224d2267d156b2b61d74
Thomas Godon <thomas.godon@gmail.com>
2024-04-25 09:43:38 +0200

    read all regsiters instead of seperate

--- AlfenHub/Alfen/Modbus/Server/AlfenModbusReader.cs
+++ AlfenHub/Alfen/Modbus/Server/AlfenModbusReader.cs
@@ -5,15 +5,17 @@ namespace AlfenHub.Alfen.Modbus.Server;
 
 internal partial class AlfenModbusClient
 {
-    // TODO: read all at once
-
-    private const int StationIdentifier = 200;
-    private const int Socket1 = 1;
-    private const int Socket2 = 2;
-
     private async Task<AlfenData> GetAlfenModbusData(CancellationToken cancellationToken)
     {
-        var sockets = await GetTotalSocketsAsync(cancellationToken);
+        var productIdentifierRegisters = await _modbusClient.ReadHoldingRegistersAsync<ushort>(200, 100, 78, cancellationToken);
+        var stationStatusRegisters = await _modbusClient.ReadHoldingRegistersAsync<ushort>(200, 1100, 5, cancellationToken);
+        var socket1Registers = Array.Empty<ushort>();
+
+
+        var productIdentifierRegistersArray = productIdentifierRegisters.ToArray();
+        var stationStatusRegistersArray = stationStatusRegisters.ToArray();
+
+        var sockets = stationStatusRegistersArray.GetSection(1105, 1).ToUshort();
         var data = new AlfenData
         {
             StationActiveMaxCurrent = await GetStationActiveMaxCurrentAsync(cancellationToken),
@@ -38,77 +40,5 @@ internal partial class AlfenModbusClient
         };
 
         return data;
-    }
-
-    private async Task<float> GetStationActiveMaxCurrentAsync(CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 1100;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<float> GetTemperatureAsync(CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 1102;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<ushort> GetTotalSocketsAsync(CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 1105;
-        const ushort count = 1;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(StationIdentifier, startingAddress, count, cancellationToken);
-        return data.ToArray()[0];
-    }
-
-    private async Task<float> GetSocketFrequencyAsync(ushort socket, CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 336;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(socket, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<float> GetSocketRealPowerPhaseL1Async(ushort socket, CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 338;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(socket, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<float> GetSocketRealPowerPhaseL2Async(ushort socket, CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 340;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(socket, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<float> GetSocketRealPowerPhaseL3Async(ushort socket, CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 342;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(socket, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private async Task<float> GetSocketModbusSlaveMaxCurrentAsync(ushort socket, CancellationToken cancellationToken)
-    {
-        const ushort startingAddress = 1210;
-        const ushort count = 2;
-        var data = await _modbusClient.ReadHoldingRegistersAsync<ushort>(socket, startingAddress, count, cancellationToken);
-        return ConvertToFloat(data);
-    }
-
-    private static float ConvertToFloat(Memory<ushort> data)
-    {
-        var bytes = BitConverter.GetBytes(data.ToArray()[1])
-            .Concat(BitConverter.GetBytes(data.ToArray()[0])).ToArray();
-        var value = BitConverter.ToSingle(bytes);
-        return value is float.NaN ? 0f : value;
     }
 }
---
+++ AlfenHub/Alfen/Modbus/Server/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿namespace AlfenHub.Alfen.Modbus.Server;
+
+internal static class ExtensionMethods
+{
+    public static ushort[] GetSection(this ushort[] input, int start, int length)
+    {
+
+    }
+
+    public static float ToFloat(this ushort[] data)
+    {
+        var bytes = BitConverter.GetBytes(data.ToArray()[1])
+            .Concat(BitConverter.GetBytes(data.ToArray()[0])).ToArray();
+        var value = BitConverter.ToSingle(bytes);
+        return value is float.NaN ? 0f : value;
+    }
+
+    public static ushort ToUshort(this ushort[] data)
+    {
+        var bytes = BitConverter.GetBytes(data.ToArray()[0]);
+        return BitConverter.ToUInt16(bytes);
+    }
+}
